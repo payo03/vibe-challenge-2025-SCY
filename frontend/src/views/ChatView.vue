1e18eda499fa2f21a1df606c8a6501abc2783468
송찬영 <52197226+payo03@users.noreply.github.com>
2025-07-05 15:49:25 +0900

    임시 Commit

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="chat-view">
-    <Chatbot />
+  <div class="popup-overlay">
+    <div class="popup-center">
+      <div class="popup-header">
+        <button @click="closeChatbotPopup" class="close-button">×</button>
+      </div>
+      <Chatbot />
+    </div>
   </div>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router'
 import Chatbot from '../components/Chatbot.vue'
+import '../styles/ChatView.css'
+
+const router = useRouter()
+function closeChatbotPopup() {
+  router.replace('/')
+}
 </script>
 
 <style scoped>
